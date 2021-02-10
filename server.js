7136e02d271d4d92900532e2e51c19e7b89ab1f7
JohnDJake <john.d.jake@gmail.com>
2021-02-09 16:46:42 -0800

    basic server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+if (process.env.NODE_ENV != "production") app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use("/api/workouts", require("./controllers/workoutsController"));
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
