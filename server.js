8d1e19c32b6e302e6956589744fdf88e6f2c64ae
JohnDJake <john.d.jake@gmail.com>
2021-02-09 21:40:45 -0800

    public static route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ if (process.env.NODE_ENV != "production") app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
